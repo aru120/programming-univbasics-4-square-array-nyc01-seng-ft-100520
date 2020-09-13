de09bc0f70609c01b4ab16d5dc53199d42d3c077
Kevin Huang <khuang159@gmail.com>
2020-09-13 20:35:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   new_num = []
   count = 0
   while count < array.length 
-    new_num[count] = Math.sqrt(array[count])
+    array[count] =  Math.sqrt(array[count])
     count += 1
   end
-  new_num
+  array
 end
